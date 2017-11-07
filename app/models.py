9a10720eeae2917c3b45951a81040de306074097
Graham Chance <graham@gtcsoftware.com>
2017-11-07 10:16:29 -0500

    Created Post model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    # todo: add poster
