68c90a1f78b327d5218ce0f2fc1df5b751d2c4e3
Graham Chance <graham@gtcsoftware.com>
2017-11-07 11:13:40 -0500

    Setup the database with all the main models

--- app/admin.py
+++ app/admin.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+from .models import Comment
+from .models import School
+
+admin.site.register(Post)
+admin.site.register(School)
+admin.site.register(Comment)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    email_domain = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
     pub_date = models.DateTimeField('date published')
-    # todo: add poster
+    poster = models.ForeignKey(User)
+    school = models.ForeignKey(School)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    commenter = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.post.title + "-" + self.commenter.username + "-" + str(self.pub_date)
